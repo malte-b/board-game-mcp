025301bff791f8e8193f601d233dfb8352b80ddd
malte-b <malte.barth01@gmail.com>
2025-06-05 00:47:42 +0200

    feat: add game details and hot games

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 import gradio as gr
-from utils.bgg import search
+from utils.bgg import get_game_details, get_hot_games, search
 
 
 search_bgg = gr.Interface(
@@ -10,4 +10,25 @@ search_bgg = gr.Interface(
     description="Search for the board games from Board Game Geek"
 )
 
-search_bgg.launch(mcp_server=True)
+game_details = gr.Interface(
+    fn=get_game_details,
+    inputs=["text"],
+    outputs="json",
+    title="Game Details",
+    description="Get detailed information for board games (comma-separated IDs)"
+)
+
+hot_games = gr.Interface(
+    fn=get_hot_games,
+    inputs=[],
+    outputs="json",
+    title="Hot Games",
+    description="Get the list of the top 50 trending games today on Board Game Geek"
+)
+
+bgg_tools = gr.TabbedInterface(
+    [search_bgg, game_details, hot_games],
+    ["Search", "Details", "Hot Games"]
+)
+
+bgg_tools.launch(mcp_server=True)
